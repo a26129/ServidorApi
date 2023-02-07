9f108e16ee516d109d2a875ef30b588905c3fbe7
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-07 19:48:31 +0100

    asdkfa

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,7 @@
 namespace ApiPrueba.Models{
     public class Usuario
-    {
+    { 
+        [key]
         public int id {get;set;}
         public string nombre{get;set;}
         public string apellido {get;set;}
